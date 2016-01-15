444db6910228696b1de8b3fdc4285fb62c104866
Michael Erne <michael.erne@gmail.com>
2016-01-15 17:55:18 +0100

    adds build logic for SOs, adds openshift compatible start script

--- sm/admin.py
+++ sm/admin.py
@@ -1,6 +1,7 @@
 from flask import Flask
 import requests
 import json
+import time
 
 from pymongo import MongoClient
 from sm.config import CONFIG, CONFIG_PATH
@@ -14,6 +15,8 @@ if not stg_path:
     raise RuntimeError('No STG specified in the configuration file.')
 with open(stg_path) as stg_content:
     stg = json.load(stg_content)
+so_buildconfig_name = stg['service_type'].split('#')[1].replace('_', '-')
+
 service_shema = stg['service_type']
 sm_name = service_shema.split('#')[1]
 
@@ -21,9 +24,19 @@ sm_name = service_shema.split('#')[1]
 cc_url = CONFIG.get('cloud_controller', 'nb_api', False)
 if not cc_url:
     raise RuntimeError('No Cloud Controller specified in the configuration file.')
+cc_admin_url = CONFIG.get('cloud_controller', 'nb_admin_api', False)
+if not cc_admin_url:
+    raise RuntimeError('No Cloud Controller Admin URL specified in the configuration file.')
 db_host = CONFIG.get('mongo', 'host', False)
 if not db_host:
     raise RuntimeError('No MongoDB host specified in the configuration file.')
+
+
+def print_response(response):
+    if response.content:
+        print '> %i: %s' % (response.status_code, response.content)
+    else:
+        print '> %i' % response.status_code
 
 
 def get_mongo_connection():
@@ -37,14 +50,49 @@ def home():
     return '', 200
 
 
-# curl -X POST $URL/update/self -> re-provisions CC (no effect)
-# curl -X POST $URL/update/$NAME -> re-provisions the deploymentConfig $NAME, do this after a /build/$NAME was triggered
+# curl -X POST $URL/update/self -> updates self, rebuilds / redeploys bc/dc of this sm
+# curl -X POST $URL/update/children -> propagates /update/self to all spawned SOs
 @app.route('/update/<name>', methods=['POST'])
 def update(name):
     if name == 'self':
-        response = requests.post(cc_url + '/build/%s' % sm_name)
+        print '### Updating myself...'
+        url = cc_admin_url + '/build/%s' % sm_name
+        print 'curl -v -X POST %s' % url
+        response = requests.post(url)
         return response.content, response.status_code
     elif name == 'children':
+        print '### Updating my SOs...'
+
+        # build
+        print '### Building new SO Image...'
+        url = cc_admin_url + '/build/%s' % so_buildconfig_name
+        print 'curl -v -X POST %s' % url
+        response = requests.post(url)
+        print_response(response)
+        if response.status_code != 201:
+            return response.content, response.status_code
+
+        build_name = json.loads(response.content)['build_name']
+        print '### Build %s started' % build_name
+        # poll until build is complete
+        done = False
+        while not done:
+            url = cc_admin_url + '/build/%s/%s' % (so_buildconfig_name, build_name)
+            print 'curl -v -X GET %s' % url
+            response = requests.get(url)
+            print_response(response)
+
+            if response.status_code != 200:
+                return response.content, response.status_code
+            content = json.loads(response.content)
+            if content['phase'] == 'Complete':
+                done = True
+            else:
+                time.sleep(5)
+
+        print '### Build done'
+        print '### Telling my SOs to redeploy themselves'
+        # propagate update to SOs
         mongo_resources = get_mongo_connection()
         resources = mongo_resources.find_one()
         del resources['_id']
@@ -52,10 +100,15 @@ def update(name):
         if resources is not None:
             for key, resource in resources.iteritems():
                 base_url = resource['extras']['loc']
-                url = 'http://%s:8081/update/self' % base_url
+                url = 'http://%s/update/self' % base_url
                 urls.append(url)
                 print 'curl -v -X POST %s' % url
+                response = requests.post(url)
+                print_response(response)
+                if response.status_code != 200:
+                    return response.content, response.status_code
         if len(urls) > 0:
+            print '### Update Signal sent to all SOs!'
             return json.dumps({'urls': urls}), 200
         return 'not yet implemented', 500
     else:
